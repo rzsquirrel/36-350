548bf5bb1c2bfd24b0bfc6e4519854afcc4add87
Reed Zhang <reedzhang123@gmail.com>
2018-04-17 12:47:12 -0400

    implemented model_select(covariates, responses, cutoff)

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -1,3 +1,16 @@
 generate_data = function(n, p) {
     return(list(covariates=matrix(rnorm(n*p), nrow=n, ncol=p), responses=rnorm(n)))
 }
+
+model_select = function(covariates, responses, cutoff) {
+    lm1 = lm(responses ~ covariates)
+    pvals = summary(lm1)$coefficients[,4]
+    pvals = pvals[2:length(pvals)]
+    if(sum(pvals < cutoff) == 0) {
+        return(c())
+    }
+    
+    lm2 = lm(responses ~ covariates[,pvals < cutoff])
+    pvals = summary(lm2)$coefficients[,4]
+    return(pvals)
+}
