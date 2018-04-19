9392be1a41d2cb6412360cf080799302302ffbe5
Reed Zhang <reedzhang123@gmail.com>
2018-04-19 12:22:43 -0400

    implemented run_simulation

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -12,5 +12,17 @@ model_select = function(covariates, responses, cutoff) {
     
     lm2 = lm(responses ~ covariates[,pvals < cutoff])
     pvals = summary(lm2)$coefficients[,4]
-    return(pvals)
+    return(pvals[2:length(pvals)])
 }
+
+run_simulation = function(n_trials, n, p, cutoff) {
+    pvals = vector()
+    for(i in 1:n_trials) {
+        data = generate_data(n, p)
+        pvals = append(pvals, model_select(data$covariates, data$responses, cutoff))
+    }
+    hist(pvals)
+}
+
+param_grid = expand.grid(c(100, 1000, 10000), c(10, 20, 50))
+apply(param_grid, 1, function(row) run_simulation(20, row[1], row[2], 0.05))
